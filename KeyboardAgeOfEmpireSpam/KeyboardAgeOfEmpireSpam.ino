f060ad0f10c6707549afdc9ca0ececd14e9e4787
Jonas Zeuner <jmacbook@Jonass-MBP.fritz.box>
2018-01-07 15:38:58 +0100

    Added Age of Empire 2 taunt spammer

---
+++ KeyboardAgeOfEmpireSpam/KeyboardAgeOfEmpireSpam.ino
@@ -0,0 +1,45 @@
+#include "DigiKeyboard.h"
+
+// Spam Age of Empires 2 Taunts
+// Little Digistump Programm
+// @auth = Tukea
+
+long randNumber;
+boolean started = false; 
+
+void setup() {
+  // don't need to set anything up to use DigiKeyboard
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+
+void loop() {
+  // this is generally not necessary but with some older systems it seems to
+  // prevent missing the first character after a delay:
+  DigiKeyboard.sendKeyStroke(0);
+
+  if(!started){
+    DigiKeyboard.delay(2000);
+    DigiKeyboard.print("Age of Empire 2 Spammer");
+    DigiKeyboard.delay(50);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.print("Start in 2 Seconds");
+    DigiKeyboard.delay(50);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(2000);
+    started = true;
+  }
+
+  // Generate a random number from 1 to 42
+  randNumber = random(1, 43);
+
+  // Print a random taunt
+  // Age of Empire 2 taunts overview
+  // http://ageofempires.wikia.com/wiki/Taunts
+  DigiKeyboard.print(randNumber);
+  DigiKeyboard.delay(10);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Digispark-Keyboard-Programs
 Digispark-Keyboard example programs
+===
+
+What you need:
+---
+* A Digispark Micro General USB Development Board (around 1-2$)
+* Arduino IDE
+
+Set up Arduino IDE:
+---
+* File > Preferences add Additional Board Manager URLs: http://digistump.com/package_digistump_index.json  
+* Tools > Board Manager > Install Digistump AVR Boards by Digistump
+* Tools > Board > Choose Digispark (Default - 16.5mhz)
+* Tools > Programmer > Change to USBtinyISP
+
+How to use:
+---
+1. Open Arduino IDE
+2. Set up the Arduino IDE
+3. Open the program
+4. Click Upload
+5. When console says "Plug in device now …" plug in the device
+6. Every time you plug in the Digispark the program will start automatic
+
+Programs:
+---
+Age of Empires 2 Taunt Spam
+'''
+When Digispark is plug in, it will spam random Age of Empire 2 taunt numbers.
+A overview about the Age of the 2 taunts: http://ageofempires.wikia.com/wiki/Taunts
+'''
